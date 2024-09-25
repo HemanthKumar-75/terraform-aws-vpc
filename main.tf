6014ebc16109e63804bd0f7a2fd206810476ec91
HemanthKumar-75 <hemanth4125@gmail.com>
2024-09-25 12:51:51 +0530

    Module Developement

--- main.tf
+++ main.tf
@@ -27,15 +27,15 @@ resource "aws_subnet" "public" {
   vpc_id = aws_vpc.main.id
   # cidr_block = var.public_subnet_cidrblocks[*]
   # availability_zone = local.az_names[*]
-  cidr_block        = var.public_subnet_cidrblocks[count.index]
-  availability_zone = local.az_names[count.index]
+  cidr_block              = var.public_subnet_cidrblocks[count.index]
+  availability_zone       = local.az_names[count.index]
   map_public_ip_on_launch = true
   tags = merge(
     var.common_tags,
     var.public_subnet_tags,
     {
-        Name = "${local.resources_name}-public-${local.az_names[count.index]}"
-    #   Name = local.subnet_name
+      Name = "${local.resources_name}-public-${local.az_names[count.index]}"
+      #   Name = local.subnet_name
     }
   )
 }
@@ -51,8 +51,8 @@ resource "aws_subnet" "private" {
     var.common_tags,
     var.private_subnet_tags,
     {
-        Name = "${local.resources_name}-private-${local.az_names[count.index]}"
-    #   Name = local.subnet_name
+      Name = "${local.resources_name}-private-${local.az_names[count.index]}"
+      #   Name = local.subnet_name
     }
   )
 }
@@ -68,8 +68,41 @@ resource "aws_subnet" "database" {
     var.common_tags,
     var.database_subnet_tags,
     {
-        Name = "${local.resources_name}-database-${local.az_names[count.index]}"
-    #   Name = local.subnet_name
+      Name = "${local.resources_name}-database-${local.az_names[count.index]}"
+      #   Name = local.subnet_name
     }
   )
+}
+
+# database subnet group for RDS
+resource "aws_db_subnet_group" "default" {
+  name       = local.resources_name
+  subnet_ids = aws_subnet.database[*].id
+
+  tags = merge(
+    var.common_tags,
+    var.database_subnet_tags,
+    {
+      Name = local.resources_name
+    }
+  )
+}
+
+resource "aws_eip" "NAT" {
+  domain = "vpc"
+}
+
+resource "aws_nat_gateway" "nat_gateway" {
+  allocation_id = aws_eip.NAT.id
+  subnet_id     = aws_subnet.public[0].id
+
+  tags = merge(
+    var.common_tags,
+    var.nat_gateway_tags,
+    {
+      Name = local.resources_name
+    }
+  )
+
+  depends_on = [ aws_internet_gateway.igw ]
 }

ab3680e2f33e7001c85f7286845638f6b14bc2d4
hemanth_kumar <hemanth4125@gmail.com>
2025-01-02 13:39:31 +0530

    Module Development

--- locals.tf
+++ locals.tf
@@ -1,5 +1,6 @@
 locals {
-  resources_name = "${var.project_name}-${var.envinronment}"
+  # resources_name = "${var.project_name}-${var.envinronment}"
+  resources_name = lower(replace("${var.project_name}-${var.envinronment}", "[^a-z0-9_-]", ""))
   az_names = slice(data.aws_availability_zones.available.names, 0,2)
 #   subnet_name = "${local.resources_name}-private-${local.az_names[count.index]}"
 }
